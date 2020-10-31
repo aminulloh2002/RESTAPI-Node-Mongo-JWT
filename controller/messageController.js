ad39389132632e8fee3b5b10a6074abfc61a3e5d
aminulloh2002 <aminulloh2002@gmail.com>
2020-10-31 20:22:32 +0700

    add delete message

--- controller/messageController.js
+++ controller/messageController.js
@@ -16,3 +16,34 @@ const insert = async (req,res)=>{
 }
 
 module.exports.insert = insert
+
+const getMessage = async (req,res)=>{
+
+    try {
+        const doc = await Message.find({},{__v:0})
+        res.status(200).send(doc)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+}
+
+module.exports.getMessage = getMessage
+
+const deleteMessage = async (req,res)=>{
+
+    // try {
+        Message.findByIdAndDelete(req.params.id).then(response=>{ 
+            if (response != null) {     
+                res.send('delete success!')
+            } else {
+                res.send('delete fail!')
+            }
+        }).catch(err=>{
+            res.send({err})
+        })
+    // } catch (error) {
+    //     res.status(400).send(error)
+    // }
+}
+
+module.exports.deleteMessage = deleteMessage
--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,11 +1,18 @@
 const router = require('express').Router()
 const verify = require('../controller/verifyToken')
-const {insert} = require('../controller/messageController')
+const {insert,getMessage,deleteMessage} = require('../controller/messageController')
 
 //insert
 router.post('/send',verify,(req,res)=>{
     insert(req,res)
 })
 
+router.get('/',verify,(req,res)=>{
+    getMessage(req,res)
+})
+
+router.delete('/delete/:id',verify,(req,res)=>{
+    deleteMessage(req,res)
+})
 
 module.exports = router
